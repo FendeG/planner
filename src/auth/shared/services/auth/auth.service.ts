9699647e390d7b614e518c932af02eb712edc567
FendeG <fendeg@gmail.com>
2021-03-13 09:25:26 +0100

    af

--- src/auth/shared/services/auth/auth.service.ts
+++ src/auth/shared/services/auth/auth.service.ts
@@ -1,13 +1,35 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
+import { switchMap } from 'rxjs/operators';
+
+import { Store } from '../../../../store';
+
+//import {do} from 'rxjs/operators';
+
+export interface User {
+  email: string,
+  uid: string,
+  authenticated: boolean
+}
+
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
+  // auth$ = this.af.authState.pipe(
+  //   switchMap(next => {
+  //     if(!next){
+  //       this.store.set('user',null)
+  //     }
+
+  // }));
+
   constructor(
-  private af: AngularFireAuth
+    private store: Store,
+    private af: AngularFireAuth
   ) { }
 
   createUser(email: string, password: string) {
